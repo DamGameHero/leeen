5e106e7e756aa4ea6f526130d8561a69207c447e
DamGameHero <drolededams@hotmail.fr>
2018-11-20 13:59:08 +0100

    feature scaling

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 if __name__ == '__main__':
     data = np.genfromtxt("data.csv", delimiter = ",", names = True)
-    print(data.size)
-    #plt.show()
     cnt = 0
     teta0 = 0.0
     teta1 = 0.0
@@ -21,21 +17,23 @@ if __name__ == '__main__':
     while cnt < 350:
         tmp0 = teta0
         tmp1 = teta1
-        #print("multiply", np.multiply(tmp1, data['km']))
-        #print("add", np.add(tmp0, np.multiply(tmp1, data['km'])))
-        #print("sub", np.subtract(np.add(tmp0, np.multiply(tmp1, data['km'])), data['price']))
-        #print("sum", np.sum(np.subtract(np.add(tmp0, np.multiply(tmp1, data['km'])), data['price'])))
-        #print((lRate / float(data.size)) * (np.sum(np.subtract(np.add(tmp0, np.multiply(tmp1, data['km'])), data['price']))))
         teta0 = tmp0 - (lRate / float(data.size)) * (np.sum(np.subtract(np.add(tmp0, np.multiply(tmp1, knew)), data['price'])))
         teta1 = tmp1 - (lRate / float(data.size)) * (np.sum(np.multiply(np.subtract(np.add(tmp0, np.multiply(tmp1, knew)), data['price']), knew)))
-        #print(teta0, teta1)
         cost.append((1 / (2 * float(data.size))) * (np.sum(np.power(np.subtract(np.add(teta0, np.multiply(teta1, knew)), data['price']), 2))))
         cnt += 1
 
     print("lRate = ", lRate / float(data.size))
-    print("cost", cost)
+    #print("cost", cost)
     print(teta0, teta1)
+    print(kaverage)
+    print(krange)
+    #teta0f = 8499
+    #teta1f = -0.02149
+    teta0f = teta0 + teta1 * ((-1 * kaverage) / krange)
+    teta1f = teta0 + teta1 * ((1 - kaverage) / krange) - teta0f
+    print(teta0f, teta1f)
     plt.plot(data['km'], data['price'], "ro")
     plt.plot(data['km'], np.add(np.multiply(teta1, knew), teta0))
+    plt.plot(data['km'], np.add(np.multiply(teta1f, data['km']), teta0f))
     #plt.plot(np.arange(cnt + 1),cost)
     plt.show()
